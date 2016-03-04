81096195bdb91be03c770fb5fecddc3b3e9bbd65
Pablo Puga <pablo.puga.git@gmail.com>
2016-03-04 17:43:52 +0100

    Updated the downloadPuppetModules.sh script to download the example42-puppi, the example42-yum and the example42-apache modules

--- scripts/downloadPuppetModules.sh
+++ scripts/downloadPuppetModules.sh
@@ -5,6 +5,12 @@ safeveyModule='safeveypuppet'
 
 yum install -y git
 
-git submodule update --init --recursive
+cd ${dir}${safeveyModule}
+git pull --rebase
+cd -
+
+puppet module install example42-puppi --modulepath $dir --version 2.1.12;
+puppet module install example42-yum --modulepath $dir --version 2.1.26;
+puppet module install example42-apache --modulepath $dir --version 2.1.12;
 
 exit 0;
